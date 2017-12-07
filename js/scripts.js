11d87c578a8548244ac2b6a5cde3c3d6c31d4c8c
Emily Watkins and Joe McKinney <Guest@Epicodus-9D.local>
2017-12-06 20:37:20 -0800

    add turn and total scores.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
-function Player(name, roll, score) {
+function Player(name, roll, turn, total) {
   this.name = name;
   this.roll = roll;
-  this.score = score;
+  this.turn = turn;
+  this.total = total;
 };
 
 Player.prototype.newRoll = function() {
@@ -11,38 +12,45 @@ Player.prototype.newRoll = function() {
 
 Player.prototype.addRoll = function() {
   if (this.roll === 1) {
-    this.roll === 0;
+    this.turn = 0;
   } else {
-    this.score += this.roll;
+    this.turn += this.roll;
   }
 };
 
+Player.prototype.addTurn = function() {
+      this.total += this.turn;
+      this.turn = 0;
+};
+
 $(document).ready(function() {
-  var playerOne = new Player(name, 0, 0);
-  var playerTwo = new Player(name, 0, 0);
+  var playerOne = new Player(name, 0, 0, 0);
+  var playerTwo = new Player(name, 0, 0, 0);
 
-
+  // populates dice roll for player 1 and adds to turn score
   $("#rollOne").click(function() {
     playerOne.newRoll();
     playerOne.addRoll();
 
     $(".roll-player1").text(playerOne.roll);
-    $(".total-player1").text(playerOne.score);
+    $(".turn-player1").text(playerOne.turn);
 
+    // changes turn
     if (playerOne.roll === 1) {
       $("#rollOne").prop('disabled', true);
       $("#rollTwo").prop('disabled', false);
     }
 
   });
-
+  // populates dice roll for player 2 and adds to turn score
   $("#rollTwo").click(function() {
     playerTwo.newRoll();
     playerTwo.addRoll();
 
     $(".roll-player2").text(playerTwo.roll);
-    $(".total-player2").text(playerTwo.score);
+    $(".turn-player2").text(playerTwo.turn);
 
+    // changes turn
     if (playerTwo.roll === 1) {
       $("#rollTwo").prop('disabled', true);
       $("#rollOne").prop('disabled', false);
@@ -50,11 +58,19 @@ $(document).ready(function() {
 
   });
 
+  // changes turn when player 1 clicks hold
   $("#holdOne").click(function() {
-
+    playerOne.addTurn();
+    $(".total-player1").text(playerOne.total);
+    $("#rollOne").prop('disabled', true);
+    $("#rollTwo").prop('disabled', false);
   });
 
+  // changes turn when player 2 clicks hold
   $("#holdTwo").click(function() {
-
-  })
+    playerTwo.addTurn();
+    $(".total-player2").text(playerTwo.total);
+    $("#rollTwo").prop('disabled', true);
+    $("#rollOne").prop('disabled', false);
+  });
 });
